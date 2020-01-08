a45dc911cf5c930c816cac1106f16eb983c503a8
Saugata Paul <saugata.paul1010@gmail.com>
2020-01-08 19:28:42 +0530

    Add files via upload
    100 % working. Callback modification is left and sim folders needs to be created.

---
+++ pipeline/check_gpu_usage.py
@@ -0,0 +1,42 @@
+from __future__ import print_function
+import time
+import os
+import argparse
+from argparse import RawTextHelpFormatter
+import datetime
+import psutil
+
+parser = argparse.ArgumentParser(description="Utility script to Monitor NVIDIA GPU and CPU usage",formatter_class=RawTextHelpFormatter)
+parser.add_argument("-i","--interval",help="the time interval in seconds to monitor (default 1)",type=float,metavar='',default=float(1))
+parser.add_argument("-mt","--max_time",help="the max time in minutes to run the program (default None(runs indefinetly))",metavar='',default=None)
+
+args=parser.parse_args()
+interval = int(args.interval)
+if args.max_time:
+    max_time = int(args.max_time)
+else:
+    max_time = None
+
+if not max_time:
+    check_time = time.time()
+    while True:
+        time.sleep(interval)
+        os.system("clear")
+        os.system("nvidia-smi --query-gpu=gpu_name,utilization.gpu,temperature.gpu,utilization.memory --format=csv")
+        ep_time = time.time() - check_time
+        cpu_usage = float(psutil.cpu_percent())
+        cpu_memory = psutil.virtual_memory()[2] 
+        print("\n CPU % Utillization, % Memory: {}, {}".format(cpu_usage,cpu_memory))
+        print("Elapsed time : {}".format(str(datetime.timedelta(seconds=ep_time))))
+else:
+    check_time = time.time()
+    start_time = time.time()
+    while (time.time() - start_time) < max_time * 60:
+        time.sleep(interval)
+        os.system("clear")
+        os.system("nvidia-smi --query-gpu=gpu_name,utilization.gpu,temperature.gpu,utilization.memory --format=csv")
+        ep_time = time.time() - check_time
+        cpu_usage = float(psutil.cpu_percent())
+        cpu_memory = psutil.virtual_memory()[2] 
+        print("\n CPU % Utillization, % Memory: {}, {}".format(cpu_usage,cpu_memory))
+        print("Elapsed time : {}".format(str(datetime.timedelta(seconds=ep_time))))
